817ae5b6c469a599555fb790627add4deed299a6
KhyleSewpersaud <khylesewpersaud@gmail.com>
2023-08-11 19:12:41 -0400

    Initialized React app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+  <div>
+    <h1>Trip Name</h1>
+    <h2># of Guests</h2>
+
+    <form>
+      <input type="text" placeholder="Add new guest"/>
+      <button type="submit">Add</button>
+    </form>
+
+    <form>
+      <input type="text" placeholder="price"/>
+      <input type="datetime-local"/>
+      <select>
+        <option value="" disabled selected hidden>Transfer To</option>
+        <option>temp</option>
+      </select>
+
+    </form>
+  </div>
   );
 }
 
